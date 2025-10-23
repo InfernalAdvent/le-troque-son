2efcaea59d27f9412ae3ebf44741d3eec3c5b752
InfernalAdvent <nbonrepaux@gmail.com>
2025-10-23 22:54:47 +0200

    added models for every database tables

---
+++ backend/api/models/conversation.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database');
+
+const Conversation = sequelize.define('users', { 
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    annonce_id: { type: DataTypes.INTEGER, allowNull: false },
+    utilisateur_initiateur_id:{ type: DataTypes.INTEGER, allowNull: false },
+    utilisateur_receveur_id:{ type: DataTypes.INTEGER, allowNull: false },
+    date_creation: {type: DataTypes.DATE, defaultValue: DataTypes.NOW},
+    date_derniere_activite: {type: DataTypes.DATE, allowNull: false}
+});
+
+module.exports = Conversation;

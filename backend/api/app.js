d762921d0e52a458564b684e4f2c327155d6f386
InfernalAdvent <nbonrepaux@gmail.com>
2025-10-23 20:20:17 +0200

    connexion to database with sequelize

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+const sequelize = require('./database');
+const PORT = process.env.PORT || 5000;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const allowedOrigins = ['http://localhost:3000'];
 
 const app = express();
 
@@ -13,8 +18,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log("CORS Origin:", origin);
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
 
 module.exports = app;
